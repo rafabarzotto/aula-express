2ceb3c26026139e5a117ab03a57533f56a51db13
Rafael Barzotto <rafabarzotto@hotmail.com>
2015-10-24 13:26:15 -0200

    listagem

--- modules/beers/index.js
+++ modules/beers/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var Controller = require('./../controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('list', { title: 'Listagem de cervejas' });
+   Controller.list(req,res); 
+ // res.render('list', { title: 'Listagem de cervejas' });
 });
 
 module.exports = router;
